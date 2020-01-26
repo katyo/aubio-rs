4a20db8c57a0938aab9053b6025c0e5a8cf3f85c
K <kayo@illumium.org>
2020-01-26 12:53:12 +0500

    Unified pre-generated bindings
    Because bindings for specific pointer width is same it can be merged.

--- aubio-sys/src/lib.rs
+++ aubio-sys/src/lib.rs
@@ -5,38 +5,11 @@
 #[cfg(feature = "generate-bindings")]
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
-#[cfg(all(not(feature = "generate-bindings"), target_arch = "x86"))]
-include!("bindings_x86.rs");
+#[cfg(all(not(feature = "generate-bindings"), target_pointer_width = "32"))]
+include!("bindings_x32.rs");
 
-#[cfg(all(not(feature = "generate-bindings"), target_arch = "x86_64"))]
-include!("bindings_x86_64.rs");
-
-#[cfg(all(not(feature = "generate-bindings"), target_arch = "arm"))]
-include!("bindings_arm.rs");
-
-#[cfg(all(not(feature = "generate-bindings"), target_arch = "aarch64"))]
-include!("bindings_aarch64.rs");
-
-#[cfg(all(not(feature = "generate-bindings"), target_arch = "mips"))]
-include!("bindings_mips.rs");
-
-#[cfg(all(not(feature = "generate-bindings"), target_arch = "mips64"))]
-include!("bindings_mips64.rs");
-
-#[cfg(all(not(feature = "generate-bindings"), target_arch = "powerpc"))]
-include!("bindings_powerpc.rs");
-
-#[cfg(all(not(feature = "generate-bindings"), target_arch = "powerpc64"))]
-include!("bindings_powerpc64.rs");
-
-//#[cfg(all(not(feature = "generate-bindings"), target_arch = "sparc"))]
-//include!("bindings_sparc.rs");
-
-#[cfg(all(not(feature = "generate-bindings"), target_arch = "sparc64"))]
-include!("bindings_sparc64.rs");
-
-#[cfg(all(not(feature = "generate-bindings"), target_arch = "wasm32"))]
-include!("bindings_wasm32.rs");
+#[cfg(all(not(feature = "generate-bindings"), target_pointer_width = "64"))]
+include!("bindings_x64.rs");
 
 #[cfg(all(
     not(feature = "generate-bindings"),
@@ -48,7 +21,7 @@ include!("bindings_wasm32.rs");
     not(target_arch = "mips64"),
     not(target_arch = "powerpc"),
     not(target_arch = "powerpc64"),
-    //not(target_arch = "sparc"),
+    not(target_arch = "sparc"),
     not(target_arch = "sparc64"),
     not(target_arch = "wasm32"),
 ))]
--- scripts/gen_bindings.sh
+++ scripts/gen_bindings.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-targets="arm-unknown-linux-gnueabi aarch64-unknown-linux-gnu i686-unknown-linux-gnu x86_64-unknown-linux-gnu mips-unknown-linux-gnu mips64-unknown-linux-gnuabi64 powerpc-unknown-linux-gnu powerpc64-unknown-linux-gnu sparc64-unknown-linux-gnu wasm32-unknown-unknown"
+#targets="arm-unknown-linux-gnueabi aarch64-unknown-linux-gnu i686-unknown-linux-gnu x86_64-unknown-linux-gnu mips-unknown-linux-gnu mips64-unknown-linux-gnuabi64 powerpc-unknown-linux-gnu powerpc64-unknown-linux-gnu sparc64-unknown-linux-gnu wasm32-unknown-unknown"
 # blacklisted: sparc-unknown-linux-gnu
+
+targets="i686-unknown-linux-gnu x86_64-unknown-linux-gnu"
 
 cd aubio-sys
 #cargo clean
@@ -12,6 +14,8 @@ done
 
 cd ..
 
+rm -f aubio-sys/src/bindings_*.rs
+
 for target in $targets; do
-    cp target/$target/debug/build/aubio-sys-*/out/bindings.rs aubio-sys/src/bindings_$(echo $target | sed -r 's/^([^-]+).*$/\1/' | sed 's/i686/x86/').rs
+    cp target/$target/debug/build/aubio-sys-*/out/bindings.rs aubio-sys/src/bindings_$(echo $target | sed -r 's/^([^-]+).*$/\1/' | sed 's/i686/x32/' | sed 's/x86_64/x64/').rs
 done
